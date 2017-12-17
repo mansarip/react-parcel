4f8b7067839305bc80da23a10a26bb5aee2da237
Luqman <luqmanrasa@gmail.com>
2017-12-17 20:10:37 +0800

    reset

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class HelloMessage extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
+	render() {
+		return <div>Hello {this.props.name}</div>;
+	}
 }
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+ReactDOM.render(<HelloMessage name="Luqman" />, mountNode);
